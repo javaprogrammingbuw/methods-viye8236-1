22be8c875b84246ef38f6cfebb67791fca463932
unknown <justinjames005@gmail.com>
2019-05-07 18:30:35 +0200

    Final

--- Circle.java
+++ Circle.java
@@ -1,5 +1,39 @@
 
+// public class Circle {
+// 	//todo: add some methods to compute a circle's diameter, circumference and area.
+// 	//print some outputs to confirm your methods are working correctly.
+// }
+
+
+import java.lang.Math;
+import java.util.*;
+
 public class Circle {
+	public static void main(String[] args) {
+		
+	Scanner	scanner = new Scanner(System.in);
+	
+	double radius = 0.0;
+	
+		System.out.println("Enter the radius");
+		
+		radius = scanner.nextDouble();
+		 System.out.println(" Enter the radius required" +radius);
+		 	
+		  double area = Math.PI* radius* radius;
+		  System.out.println("Area of the circle" +area);
+		  
+		  double circumference = 2*Math.PI*radius;
+		  System.out.println("Circumference of the circle"+ circumference);
+		  
+			  
+		
+		
+	}
 	//todo: add some methods to compute a circle's diameter, circumference and area.
-	//print some outputs to confirm your methods are working correctly.
+	//print some outputs to confirm you methods are working right.
+
 }
+
+
+
--- Prime.java
+++ Prime.java
@@ -1,15 +1,53 @@
+
+		
+
+
+// 	//todo: write a method isPrime which tells you if a given number is a prime or not
+// 	//then, write another method findDoublette which takes an integer value as input and 
+// 	//is looking for the first prime-doublette it findes. A prime-doublette consists of
+// 	//two prime values with the difference of 2 (e.g. 3 and 5).
+// 	//The function should return the smaller one of the doublette. If there exists no
+// 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
+// 	//the function should return 0.
+		
+	
+
+
 import java.util.Scanner;
 
 
 public class Prime {
-		
-	//todo: write a method isPrime which tells you if a given number is a prime or not
-	//then, write another method findDoublette which takes an integer value as input and 
-	//is looking for the first prime-doublette it findes. A prime-doublette consists of
-	//two prime values with the difference of 2 (e.g. 3 and 5).
-	//The function should return the smaller one of the doublette. If there exists no
-	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
-	//the function should return 0.
-		
 	
-}
+	public static void main(String[] args) {
+		
+		
+		Scanner	in= new Scanner(System.in);
+	
+
+		System.out.println("Enter the number");
+		int number = in.nextInt();
+		System.out.println(isPrime(number));
+	}
+		
+		public static boolean isPrime(int number) {
+//		 isPrime = true;
+		
+		for (int i=2; i<number; i++)
+			
+		{
+			
+			if (number%i==0)
+			{
+				return false;
+			}
+		}
+			return true;
+		}
+
+		
+		
+		
+	}
+
+	
+ 
